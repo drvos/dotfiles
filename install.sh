315715cbf5dd8e50aaf48f3f0b321237153eb689
Volker Schering <mail@volker-schering.de>
2018-12-18 10:28:31 +0100

    replace ~ to ${HOME}

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 ##
 
 # vim config
-ln -sf /home/vos/dotfiles/vim /home/vos/.vim
-ln -sf /home/vos/dotfiles/vimrc /home/vos/.vimrc
+ln -sf ${HOME}/dotfiles/vim ${HOME}/.vim
+ln -sf ${HOME}/dotfiles/vimrc ${HOME}/.vimrc
+
