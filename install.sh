6d82bf83744703ca20a582cd6daac6edaf078a88
Volker Schering <mail@volker-schering.de>
2018-12-18 10:33:01 +0100

    zsh config grosse Teile davon sind aus der grml (grml.org) config übernommen

--- install.sh
+++ install.sh
@@ -6,3 +6,9 @@
 ln -sf ${HOME}/dotfiles/vim ${HOME}/.vim
 ln -sf ${HOME}/dotfiles/vimrc ${HOME}/.vimrc
 
+# zsh config
+ln -sf ${HOME}/dotfiles/zsh ${HOME}/.zsh
+ln -sf ${HOME}/dotfiles/zlogout ${HOME}/.zlogout
+ln -sf ${HOME}/dotfiles/zshrc ${HOME}/.zshrc
+ln -sf ${HOME}/dotfiles/zshrc.pre ${HOME}/.zshrc.pre
+ln -sf ${HOME}/dotfiles/zshrc.local ${HOME}/.zshrc.local
