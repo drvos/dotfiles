2951beea1457d62e22898b510856b6332b8c3283
Volker Schering <mail@volker-schering.de>
2018-12-20 15:53:02 +0100

    git config gvimrc

--- install.sh
+++ install.sh
@@ -1,14 +1,50 @@
-#!/bin/zsh
-#
-##
+#!/usr/bin/env sh
 
-# vim config
-ln -sf ${HOME}/dotfiles/vim ${HOME}/.vim
-ln -sf ${HOME}/dotfiles/vimrc ${HOME}/.vimrc
+dotfiles="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-# zsh config
-ln -sf ${HOME}/dotfiles/zsh ${HOME}/.zsh
-ln -sf ${HOME}/dotfiles/zlogout ${HOME}/.zlogout
-ln -sf ${HOME}/dotfiles/zshrc ${HOME}/.zshrc
-ln -sf ${HOME}/dotfiles/zshrc.pre ${HOME}/.zshrc.pre
-ln -sf ${HOME}/dotfiles/zshrc.local ${HOME}/.zshrc.local
+# to error out
+warn() {
+  echo "$1" >&2
+}
+
+die() {
+  warn "$1"
+  exit 1
+}
+
+lnif() {
+  if [[ ! -e $2 ]] ; then
+    ln -s $1 $2
+  fi
+}
+
+echo "Installing/Updating dotfiles...\n"
+
+# if [ ! -e $dotfiles/.git ]; then
+#   echo "Cloning dotfiles\n"
+#   git clone https://github.com/fdietz/dotfiles.git $dotfiles
+# else
+#   echo "Updating dotfiles\n"
+#   cd $dotfiles && git pull
+# fi
+
+# zsh
+echo "Setting up zsh...\n"
+lnif $dotfiles/zsh $HOME/.zsh
+lnif $dotfiles/zlogout $HOME/.zlogout
+lnif $dotfiles/zshrc $HOME/.zshrc
+lnif $dotfiles/zshrc.pre $HOME/.zshrc.pre
+lnif $dotfiles/zshrc.local $HOME/.zshrc.local
+
+# vim
+echo "Setting up vim...\n"
+lnif $dotfiles/vim $HOME/.vim
+lnif $dotfiles/vimrc $HOME/.vimrc
+lnif $dotfiles/gvimrc $HOME/.gvimrc
+lnif $dotfiles/vimrc.local $HOME/.vimrc.local
+
+# git
+echo "Setting up git...\n"
+lnif $dotfiles/gitconfig $HOME/.gitconfig
+lnif $dotfiles/gitignore_global $HOME/.gitignore_global
+
