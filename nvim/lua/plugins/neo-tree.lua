9ee9018af40f7c6514e4af3300380d5fd4a58b3f
Volker Schering <mail@volker-schering.de>
2024-06-02 11:04:06 +0200

    Plugin neo-tree

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,23 @@
+-- Plugin neo-tree
+return {
+   { "nvim-neo-tree/neo-tree.nvim", 
+      name = "neo-tree", 
+      priority = 1000,
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		keys = {
+			{ "<leader>o", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+		},
+		config = function()
+			require("neo-tree").setup({
+				close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+				popup_border_style = "rounded",
+				enable_git_status = true,
+			})
+		end,
+   }
+}
