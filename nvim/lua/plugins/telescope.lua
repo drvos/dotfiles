1ac898965d20a90155783853a1486d04958556f0
Volker Schering <mail@volker-schering.de>
2024-06-02 15:02:36 +0200

    Plugin telescope

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,20 @@
+-- Plugin telescope
+return {
+	{ 
+		"nvim-telescope/telescope.nvim", 
+		name = "telescope",
+		priority = 1000,
+      branch = '0.1.x',
+		dependencies = { 
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<leader>p", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
+			{ "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Telescope buffers" },
+			{ "<leader>g", "<cmd>Telescope live_grep<cr>", desc = "Telescope live grep" },
+		},
+		config = function()
+			require("telescope").setup({ })
+		end,
+	}
+}
