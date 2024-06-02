9898facb613b51b7dce120051788411d484c65d8
Volker Schering <mail@volker-schering.de>
2024-06-02 11:02:47 +0200

    Lazy package manager

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,3 +4,21 @@ vim.cmd("set softtabstop=3")
 vim.cmd("set shiftwidth=3")
 
 vim.g.mapleader = " "
+
+--------------------------------------------------------------------------------
+-- --> Plugin package manager
+--------------------------------------------------------------------------------
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins") -- Plugins from directory lua/plugins
