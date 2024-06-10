e1a3b92468458de4dd918969c99da5c6880e96c5
Volker Schering <mail@volker-schering.de>
2024-06-10 20:18:38 +0200

    Added Plugin lsp-config

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,31 @@
+return {
+   {
+      "williamboman/mason.nvim",
+      name = "mason",
+      config = function()
+         require("mason").setup()
+      end,
+   },
+   {
+      "williamboman/mason-lspconfig.nvim",
+      name = "mason-lspconfig",
+      config = function()
+         require("mason-lspconfig").setup({
+            ensure_installed = { "lua_ls", "ansiblels" }
+         })
+      end,
+   },
+   {
+      "neovim/nvim-lspconfig",
+      name = "nvim-lspconfig",
+      config = function()
+         local lspconfig = require("lspconfig")
+         lspconfig.lua_ls.setup({})
+         lspconfig.ansiblels.setup({})
+
+         vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+         vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+      end,
+   }
+}
