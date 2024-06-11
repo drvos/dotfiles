dfec6061c2579f0c30d2646b285485b0fe0b1ea3
Volker Schering <mail@volker-schering.de>
2024-06-11 23:07:04 +0200

    Lsp keys

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -23,9 +23,10 @@ return {
          lspconfig.lua_ls.setup({})
          lspconfig.perlpls.setup({})
 
-         vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-         vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
       end,
    }
 }
