3f2aa915e128f5c9ab96c9ff4bf87ec828d76d74
Volker Schering <mail@volker-schering.de>
2024-06-11 23:04:24 +0200

    Remove trailling spaces

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,11 +1,10 @@
 return {
    {
-      "catppuccin/nvim", 
-      name = "catppuccin", 
+      "catppuccin/nvim",
+      name = "catppuccin",
       priority = 1000,
       config = function()
          require("catppuccin").setup({
-           vim.cmd.colorscheme "catppuccin" 
          })
       end,
    }
