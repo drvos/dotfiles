1d117395ec1d629e2ab3e3616fed7fc21828a752
Volker Schering <mail@volker-schering.de>
2024-06-30 07:32:14 +0200

    Remove trailing spaces

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,11 +2,11 @@ return {
    {
       "nvim-lualine/lualine.nvim",
       name = "lualine",
-      dependencies = { 
+      dependencies = {
          "nvim-tree/nvim-web-devicons"
       },
       config = function()
-         require("lualine").setup({ 
+         require("lualine").setup({
             options = { theme = 'dracula' }
          })
       end,
